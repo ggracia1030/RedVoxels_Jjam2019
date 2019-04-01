9a13140906f5bdaddd55f455dc4d9d7960884654
Guillem Gracia <guillemgracia99@gmail.com>
2019-04-01 15:48:49 +0200

    First commit

---
+++ Assets/Scripts/CameraEffects.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEffects : MonoBehaviour
+{
+    GameObject mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetCamera();
+    }
+
+    void ResetCamera()
+    {
+        mainCamera = Camera.main.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ShakeCamera(float time)
+    {
+
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : Singleton<InputManager>
+{
+    public enum Actions { };
+    public enum KeyBoardButtons { };
+    public enum GamePadButtons { };
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public bool GetButtonDown(string btn)
+    {
+        
+        return Input.GetButtonDown(btn);
+    }
+
+    public bool GetButton(string btn)
+    {
+        return Input.GetButton(btn);
+    }
+
+    public bool GetButtonUp(string btn)
+    {
+        return Input.GetButtonUp(btn);
+    }
+
+    public float GetAxis(string axis)
+    {
+        return Input.GetAxis(axis);
+    }
+}
