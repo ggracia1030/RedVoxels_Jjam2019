0a0645bb8a47de11d68c4d8e71479b52efc82c91
Eric Sanchez <esanchezverges@gmail.com>
2019-04-01 16:39:49 +0200

    Player Movement - Movimiento basico

--- Assets/Scripts/PlayerControllers/PlayerGame.cs
+++ Assets/Scripts/PlayerControllers/PlayerGame.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class PlayerGame : MonoBehaviour
 {
+    Transform tf;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tf = GetComponent<Transform>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovement();
     }
 
     void PlayerMovement()
     {
-        InputManager.Instance.GetAxis("vertical");
-        InputManager.Instance.GetAxis("horitzontal");
+        tf.position = new Vector3(tf.position.x + (InputManager.Instance.GetAxis("Horizontal")* Time.deltaTime * speed), tf.position.y , tf.position.z + (InputManager.Instance.GetAxis("Vertical") * Time.deltaTime * speed));
+       
+
+
     }
 }
