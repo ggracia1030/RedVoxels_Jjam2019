b48ce4d013d76cc806d1db161d69d7296a3e1e15
Guillem Gracia <guillemgracia99@gmail.com>
2019-04-01 21:18:34 +0200

    Animaciones y prefabs

--- Assets/Scripts/Generic/GameManager.cs
+++ Assets/Scripts/Generic/GameManager.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
-
     public enum GameState { MainMenu, InGame, Pause, GameOver};
     GameState gameState;
     int currentLevel;
@@ -27,10 +26,7 @@ public class GameManager : Singleton<GameManager>
     {
         gameState = _gameState;
         currentLevel = level;
-        if(gameState == GameState.InGame)
-        {
-            SceneManager.LoadScene("Level" + level);
-        }
+        StartCoroutine(FadeOutCamera(_gameState, level));
     }
 
     public void ChangeToLevel(int level)
@@ -59,5 +55,37 @@ public class GameManager : Singleton<GameManager>
             Time.timeScale = 1;
         }
         GameObject.Find("PauseMenu").GetComponent<ActivatePauseMenu>().ActivePauseMenu(_paused);
+    }
+
+    IEnumerator FadeOutCamera(GameState _gameState, int level = 0)
+    {
+        float maxTime = 1;
+        CanvasGroup mainCanvas = GameObject.Find("MainCanvas").GetComponent<CanvasGroup>();
+        for(float time = 0; time < maxTime; time+=Time.deltaTime)
+        {
+            mainCanvas.alpha = time / maxTime;
+            yield return null;
+        }
+        if (gameState == GameState.InGame)
+        {
+            SceneManager.LoadScene("Level" + level);
+        }
+        else
+        {
+            SceneManager.LoadScene(_gameState.ToString());
+        }
+        yield return null;
+    }
+
+    IEnumerator FadeInCamera()
+    {
+        float maxTime = 1;
+        CanvasGroup mainCanvas = GameObject.Find("MainCanvas").GetComponent<CanvasGroup>();
+        for (float time = maxTime; time > 0; time -= Time.deltaTime)
+        {
+            mainCanvas.alpha = time / maxTime;
+            yield return null;
+        }
+        mainCanvas.alpha = 0;
     }
 }
