bf1e87b4a1218b4bb5a74219db4faf06f1be9996
Eric Sanchez <esanchezverges@gmail.com>
2019-04-01 16:28:38 +0200

    Empiezo player controller

--- Assets/Scripts/PlayerControllers/PlayerGame.cs
+++ Assets/Scripts/PlayerControllers/PlayerGame.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerGame : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,11 @@ public class PlayerGame : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void PlayerMovement()
+    {
+        InputManager.Instance.GetAxis("vertical");
+        InputManager.Instance.GetAxis("horitzontal");
     }
 }
