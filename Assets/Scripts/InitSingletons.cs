0bfa6122b9999388a7cfd0bc39ee21a254e0c9d8
Guillem Gracia <guillemgracia99@gmail.com>
2019-04-01 16:11:23 +0200

    Game Manager and init singletons

---
+++ Assets/Scripts/InitSingletons.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitSingletons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        InputManager.Instance.Init();
+        GameManager.Instance.Init();
+    }
+}
