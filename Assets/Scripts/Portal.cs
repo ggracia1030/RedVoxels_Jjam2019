c9bf2fdf0133ca44fd184accf7e68c84fb6f5c33
Teteju <jesusjavier.maldonado@balmes.epiaedu.cat>
2019-04-02 20:00:27 +0200

    added level 4

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+
+
+    public Transform SpawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.gameObject.tag == "Player")
+        {
+
+            other.transform.position = SpawnPoint.position;
+
+        }
+
+    }
+
+}
